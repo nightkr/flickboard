8b6840f27bfe4729e9b23bd08b09a2b749d71b0d
Natalie Klestrup Röijezon <nat@nullable.se>
2024-02-06 08:45:08 +0100

    Tweaks

--- app/src/main/java/se/nullable/flickboard/MainActivity.kt
+++ app/src/main/java/se/nullable/flickboard/MainActivity.kt
@@ -5,17 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import se.nullable.flickboard.ui.KeyboardPreview
 import se.nullable.flickboard.ui.Settings
 import se.nullable.flickboard.ui.theme.FlickBoardTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,27 +28,12 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
+                        TopAppBar(title = { Text(stringResource(id = R.string.app_name)) })
                         KeyboardPreview()
                         Settings()
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlickBoardTheme {
-        Greeting("Android")
     }
 }
