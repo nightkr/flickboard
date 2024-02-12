2084c64f6d82002b5a065f2998222a3a63d25891
Natalie Klestrup Röijezon <nat@nullable.se>
2024-02-12 18:22:52 +0100

    Make emoji tabs narrower, theming

--- app/src/main/java/se/nullable/flickboard/ui/emoji/EmojiKeyboard.kt
+++ app/src/main/java/se/nullable/flickboard/ui/emoji/EmojiKeyboard.kt
@@ -1,32 +1,40 @@
 package se.nullable.flickboard.ui.emoji
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.res.getResourceIdOrThrow
 import se.nullable.flickboard.R
@@ -37,30 +45,42 @@ import androidx.emoji2.emojipicker.R as Emoji2R
 fun EmojiKeyboard(onAction: (Action) -> Unit) {
     val emojis = emojiList()
     var selectedCategoryIndex by remember(emojis) { mutableStateOf(0) }
+    val tabScrollState = rememberScrollState()
     val emojiSize = 32.dp
     val emojiPadding = 8.dp
     val emojiItemSize = emojiSize + emojiPadding * 3
     val emojiSizeSp = with(LocalDensity.current) { emojiSize.toSp() }
     Column {
-        Row {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
+        ) {
             Button(onClick = { onAction(Action.ToggleEmojiMode) }, Modifier.padding(8.dp)) {
                 Icon(Icons.AutoMirrored.Filled.ArrowBack, "Return to keyboard")
             }
-            ScrollableTabRow(
-                selectedTabIndex = selectedCategoryIndex,
-                edgePadding = 0.dp,
-                modifier = Modifier.weight(1F)
+            // Ideally this would be a ScrollableTabRow, but it enforces a massive minimum width
+            Row(
+                modifier = Modifier
+                    .weight(1F)
+                    .horizontalScroll(tabScrollState)
             ) {
                 emojis.categories.forEachIndexed { index, emojiCategory ->
                     Tab(
                         selected = selectedCategoryIndex == index,
                         onClick = { selectedCategoryIndex = index },
                         icon = {
-                            Icon(
-                                painterResource(emojiCategory.iconId),
-                                contentDescription = null
-                            )
+                            Box {
+                                Icon(
+                                    painterResource(emojiCategory.iconId),
+                                    contentDescription = null,
+                                    Modifier.padding(8.dp)
+                                )
+                                if (selectedCategoryIndex == index) {
+                                    TabRowDefaults.SecondaryIndicator(Modifier.align(Alignment.BottomCenter))
+                                }
+                            }
                         },
+                        modifier = Modifier.width(IntrinsicSize.Max)
                     )
                 }
             }
@@ -71,7 +91,9 @@ fun EmojiKeyboard(onAction: (Action) -> Unit) {
         LazyVerticalGrid(
             columns = GridCells.Adaptive(emojiItemSize),
             verticalArrangement = Arrangement.Center,
-            modifier = Modifier.heightIn(max = 200.dp)
+            modifier = Modifier
+                .heightIn(max = 200.dp)
+                .background(MaterialTheme.colorScheme.surface)
         ) {
             items(emojis.categories[selectedCategoryIndex].emojis) {
                 val primaryVariant = it.variants[0]
@@ -85,6 +107,12 @@ fun EmojiKeyboard(onAction: (Action) -> Unit) {
             }
         }
     }
+}
+
+@Composable
+@Preview
+fun EmojiKeyboardPreview() {
+    EmojiKeyboard(onAction = {})
 }
 
 @SuppressLint("PrivateResource")
