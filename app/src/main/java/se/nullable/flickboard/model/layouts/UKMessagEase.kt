e6db5c8d63213c81aca162a1fd933719ab4dc388
Yuri Khan <yuri.v.khan@gmail.com>
2024-02-14 02:37:23 +0700

    Refactor Ukrainian layout as overrides over Russian

--- app/src/main/java/se/nullable/flickboard/model/layouts/UKMessagEase.kt
+++ app/src/main/java/se/nullable/flickboard/model/layouts/UKMessagEase.kt
@@ -7,89 +7,40 @@ import se.nullable.flickboard.model.Direction
 import se.nullable.flickboard.model.KeyM
 import se.nullable.flickboard.model.Layer
 import se.nullable.flickboard.model.Layout
+import se.nullable.flickboard.model.layouts.RU_MESSAGEASE_MAIN_LAYER
 import se.nullable.flickboard.ui.FlickBoardParent
 import se.nullable.flickboard.ui.Keyboard
 
 val UK_MESSAGEASE_MAIN_LAYER = Layer(
     keyRows = listOf(
         listOf(
-            KeyM(
-                actions = mapOf(
-                    Direction.CENTER to Action.Text("с"),
-                    Direction.BOTTOM to Action.Text("ц"),
-                    Direction.BOTTOM_RIGHT to Action.Text("п")
-                )
-            ),
-            KeyM(
-                actions = mapOf(
-                    Direction.TOP to Action.Text("й"),
-                    Direction.CENTER to Action.Text("и"),
-                    Direction.BOTTOM to Action.Text("к")
-                )
-            ),
-            KeyM(
-                actions = mapOf(
-                    Direction.CENTER to Action.Text("т"),
-                    Direction.BOTTOM_LEFT to Action.Text("ь")
-                )
-            ),
+            KeyM(actions = mapOf()),
+            KeyM(actions = mapOf()),
+            KeyM(actions = mapOf()),
         ),
         listOf(
             KeyM(
                 actions = mapOf(
-                    Direction.CENTER to Action.Text("в"),
-                    Direction.TOP to Action.Text("б"),
                     Direction.RIGHT to Action.Text("і"),
                     Direction.BOTTOM to Action.Text("ґ")
                 )
             ),
-            KeyM(
-                actions = mapOf(
-                    Direction.CENTER to Action.Text("о"),
-                    Direction.TOP_LEFT to Action.Text("ч"),
-                    Direction.TOP to Action.Text("м"),
-                    Direction.TOP_RIGHT to Action.Text("х"),
-                    Direction.LEFT to Action.Text("ж"),
-                    Direction.RIGHT to Action.Text("г"),
-                    Direction.BOTTOM_LEFT to Action.Text("щ"),
-                    Direction.BOTTOM to Action.Text("я"),
-                    Direction.BOTTOM_RIGHT to Action.Text("ш")
-                )
-            ),
-            KeyM(
-                actions = mapOf(
-                    Direction.CENTER to Action.Text("а"),
-                    Direction.LEFT to Action.Text("л")
-                )
-            ),
+            KeyM(actions = mapOf()),
+            KeyM(actions = mapOf()),
         ),
         listOf(
             KeyM(
                 actions = mapOf(
-                    Direction.CENTER to Action.Text("е"),
                     Direction.TOP to Action.Text("ї"),
-                    Direction.TOP_RIGHT to Action.Text("д"),
                     Direction.RIGHT to Action.Text("є")
                 )
             ),
-            KeyM(
-                actions = mapOf(
-                    Direction.CENTER to Action.Text("р"),
-                    Direction.LEFT to Action.Text("ю"),
-                    Direction.TOP to Action.Text("у"),
-                    Direction.RIGHT to Action.Text("з")
-                )
-            ),
-            KeyM(
-                actions = mapOf(
-                    Direction.CENTER to Action.Text("н"),
-                    Direction.TOP_LEFT to Action.Text("ф")
-                )
-            ),
+            KeyM(actions = mapOf()),
+            KeyM(actions = mapOf()),
         ),
         listOf(SPACE)
     )
-)
+).mergeFallback(RU_MESSAGEASE_MAIN_LAYER)
 
 val UK_MESSAGEASE = Layout(
     mainLayer = UK_MESSAGEASE_MAIN_LAYER,
