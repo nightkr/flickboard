1979282a86ad77d78319596dba70e602da6fb8d7
allthelazorz <94384416+allthelazorz@users.noreply.github.com>
2024-04-02 10:38:37 +0200

    Added Hungarian layouts to Settings.kt

--- app/src/main/java/se/nullable/flickboard/ui/Settings.kt
+++ app/src/main/java/se/nullable/flickboard/ui/Settings.kt
@@ -90,6 +90,8 @@ import se.nullable.flickboard.model.layouts.EN_MESSAGEASE
 import se.nullable.flickboard.model.layouts.ES_MESSAGEASE
 import se.nullable.flickboard.model.layouts.FR_EXT_MESSAGEASE
 import se.nullable.flickboard.model.layouts.FR_MESSAGEASE
+import se.nullable.flickboard.model.layouts.HU_MESSAGEASE
+import se.nullable.flickboard.model.layouts.HU_UUP_MESSAGEASE
 import se.nullable.flickboard.model.layouts.MESSAGEASE_NUMERIC_CALCULATOR_LAYER
 import se.nullable.flickboard.model.layouts.MESSAGEASE_NUMERIC_PHONE_LAYER
 import se.nullable.flickboard.model.layouts.RU_MESSAGEASE
@@ -968,6 +970,8 @@ enum class LetterLayerOption(override val label: String, val layout: Layout) : L
     English("English (MessagEase)", EN_MESSAGEASE),
     German("German (MessagEase)", DE_MESSAGEASE),
     GermanEnglish("German/English (MessagEase)", EN_DE_MESSAGEASE),
+    Hungarian("Hungarian (MessagEase)", HU_MESSAGEASE),
+    HungarianUUp("Hungarian (MessagEase, U always up)", HU_UUP_MESSAGEASE),
     Russian("Russian (MessagEase)", RU_MESSAGEASE),
     RussianPhonetic("Russian phonetic (MessagEase)", RU_PHONETIC_MESSAGEASE),
     Spanish("Spanish (MessagEase)", ES_MESSAGEASE),
