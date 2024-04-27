6af08742c92c3836e65b2bb9cd37f5dc1624328c
Natalie Klestrup Röijezon <nat@nullable.se>
2024-04-28 01:32:07 +0200

    Add icons to word shift action

--- app/src/main/java/se/nullable/flickboard/model/KeyM.kt
+++ app/src/main/java/se/nullable/flickboard/model/KeyM.kt
@@ -179,7 +179,10 @@ sealed class Action {
      * Shift the case for the current word up/down one step (lower <-> Title <-> UPPER )
      */
     data class ToggleWordCase(val direction: CaseChangeDirection) : Action() {
-        override fun visual(modifier: ModifierState?): ActionVisual = ActionVisual.None
+        override fun visual(modifier: ModifierState?): ActionVisual = when (direction) {
+            CaseChangeDirection.Up -> ActionVisual.Icon(R.drawable.baseline_keyboard_double_arrow_up_24)
+            CaseChangeDirection.Down -> ActionVisual.Icon(R.drawable.baseline_keyboard_double_arrow_down_24)
+        }
     }
 
     data object ToggleCtrl : Action() {
