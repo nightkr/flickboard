3d2807f1b9499623d5769aa8aa95e0a3b5667956
MisterE <mistere123.coding@gmail.com>
2024-04-30 23:27:46 -0400

    Add pointer trail color picker

--- app/src/main/java/se/nullable/flickboard/ui/Keyboard.kt
+++ app/src/main/java/se/nullable/flickboard/ui/Keyboard.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
