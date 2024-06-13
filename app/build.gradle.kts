0fafa8413d052f26182b7831474dfa6eebdaa13c
Natalie Klestrup Röijezon <nat@nullable.se>
2024-06-13 22:27:15 +0200

    Release v0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "se.nullable.flickboard"
         minSdk = 28
         targetSdk = 34
-        versionCode = 20
-        versionName = "0.1.3"
+        versionCode = 21
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
