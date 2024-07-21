024384bfc1bdc5ca4b84f3cab5aae02b7a70a3a4
Tirbod <65743522+Tirbod@users.noreply.github.com>
2024-07-22 02:02:27 +0330

    Update FAMessagEase.kt

--- app/src/main/java/se/nullable/flickboard/model/layouts/FAMessagEase.kt
+++ app/src/main/java/se/nullable/flickboard/model/layouts/FAMessagEase.kt
@@ -9,8 +9,7 @@ import se.nullable.flickboard.model.KeyM
 import se.nullable.flickboard.model.Layer
 import se.nullable.flickboard.model.Layout
 //import se.nullable.flickboard.model.TextDirection
-import se.nullable.flickboard.ui.FlickBoardParent
-import se.nullable.flickboard.ui.Keyboard
+import se.nullable.flickboard.ui.KeyboardLayoutPreview
 
 val FA_MESSAGEASE_MAIN_LAYER = Layer(
     keyRows = listOf(
@@ -114,21 +113,18 @@ val FA_MESSAGEASE_MAIN_LAYER = Layer(
 val FA_MESSAGEASE = Layout(
     mainLayer = FA_MESSAGEASE_MAIN_LAYER,
     controlLayer = CONTROL_MESSAGEASE_LAYER,
+    digits = "۰۱۲۳۴۵۶۷۸۹",
 //    textDirection = TextDirection.RightToLeft,
 )
 
 @Composable
 @Preview
-fun FaMessagEasePreview() {
-    FlickBoardParent {
-        Keyboard(layout = Layout(FA_MESSAGEASE_MAIN_LAYER), onAction = {})
-    }
+fun FaMessagEaseKeyboardPreview() {
+    KeyboardLayoutPreview(layout = Layout(FA_MESSAGEASE_MAIN_LAYER))
 }
 
 @Composable
 @Preview
 fun FaMessagEaseFullKeyboardPreview() {
-    FlickBoardParent {
-        Keyboard(layout = FA_MESSAGEASE, showAllModifiers = true, onAction = {})
-    }
+    KeyboardLayoutPreview(layout = FA_MESSAGEASE, showAllModifiers = true)
 }
