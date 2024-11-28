e83ac67ad451d780c8dfe22a6ea0ec327be4b982
Natalie Klestrup Röijezon <nat@nullable.se>
2024-11-28 23:26:22 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         }
     }
     namespace = "se.nullable.flickboard"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "se.nullable.flickboard"
@@ -103,10 +103,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    implementation("androidx.activity:activity-compose:1.9.1")
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -114,15 +114,15 @@ dependencies {
     // Should be included transitively, but doesn't actually seem to render emojis
     // without a direct dependency.
     // Emojis still not rendering properly? Try gradle clean and then rebuilding.
-    implementation("androidx.emoji2:emoji2:1.4.0")
-    implementation("androidx.emoji2:emoji2-emojipicker:1.4.0")
-    implementation("androidx.emoji2:emoji2-bundled:1.4.0")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.emoji2:emoji2:1.5.0")
+    implementation("androidx.emoji2:emoji2-emojipicker:1.5.0")
+    implementation("androidx.emoji2:emoji2-bundled:1.5.0")
+    implementation("androidx.navigation:navigation-compose:2.8.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.11.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("tools.fastlane:screengrab:2.1.1")
     debugImplementation("androidx.compose.ui:ui-tooling")
