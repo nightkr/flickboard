508c7be2d8d8a21e8010e3ffc368f00147b5cc64
Natalie Klestrup Röijezon <nat@nullable.se>
2024-12-17 17:10:31 +0100

    v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "se.nullable.flickboard"
         minSdk = 28
         targetSdk = 35
-        versionCode = 55
-        versionName = "0.2.0-beta5"
+        versionCode = 56
+        versionName = "0.2.0"
         base.archivesName = "flickboard-v${versionName}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
