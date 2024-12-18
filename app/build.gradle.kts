622aae5b752115b60e87b92cc864bf0ef7419b5c
Natalie Klestrup Röijezon <nat@nullable.se>
2024-12-18 09:30:44 +0100

    Downgrade JVM target to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,11 +84,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "19"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
